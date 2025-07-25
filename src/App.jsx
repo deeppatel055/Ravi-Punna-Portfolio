a5438068da6642946614880933a47632d0c8ba6f
deepatel055 <deeppatel2gmail.com>
2025-07-25 20:56:54 +0530

    Add 404 fallback for GitHub

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route } from 'react-router-dom';
 import HeroSection from './Component/Hero-Section/HeroSection'
 import Navbar from './Component/Navbar/Navbar'
 import Project1 from './Pages/Project1';
